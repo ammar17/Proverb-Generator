1e6143f9129b299f5c573fc44b818a3fa4ccda8b
baviereteam <cyrilremy.grageon@gmail.com>
2018-01-13 03:40:08 +0100

    Make images accessible.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" title="Live on tape from the Ed Sullivan Theater in New York City, it's Stephen Colbert !" />
           <h1 className="App-title">Alternative Proverb Generator</h1>
         </header>
           <ProverbView />
